e869084f6d283706cf15da8b1c6c352004f3bbe9
andre <andrecabralpires@gmail.com>
2021-11-12 10:56:03 +0100

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require "byebug"
+puts "cleanig DB"
+Article.destroy_all
+
+
+puts "creating Articles"
+
+3.times do
+  file = URI.open('https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2020/06/playstation-5-1964465.jpg?itok=EXk3Upcm')
+  article = Article.new(title: 'PS5', body: "A great console")
+  article.photo.attach(io: file, filename: 'ps5.png', content_type: 'image/png')
+  article.save
+  byebug
+end
+
+puts "done"
